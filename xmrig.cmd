38b288071534a4845cc39add9fa794532a9d0b8c
Oleg Ivanilov <equuleus@mail.ru>
2017-11-16 18:41:24 +0300

    Update xmrig.cmd

--- xmrig.cmd
+++ xmrig.cmd
@@ -6,7 +6,6 @@ TITLE XMRig
 SET URL=pool.monero.hashvault.pro
 SET PORT=5555
 SET WALLET=4...
-SET DIFF=10000
 SET ID=%COMPUTERNAME%
 SET EMAIL=user@server.com
 
@@ -16,14 +15,35 @@ SET SRC=%~dp0
 SET PROGRAM_TITLE=XMRig
 SET PROGRAM_PATH=%SRC:~0,-1%
 SET PROGRAM_CPU_FILENAME=xmrig.exe
-SET PROGRAM_GPU_FILENAME=xmrig-nvidia.exe
+SET PROGRAM_NVIDIA_FILENAME=xmrig-nvidia.exe
+SET PROGRAM_AMD_FILENAME=xmrig-amd.exe
 SET PROGRAM_PARAMETERS=--algo=cryptonight --url=%URL%:%PORT% --keepalive --retries=5 --retry-pause=5 --donate-level=1 --nicehash 
-SET PROGRAM_CPU_PARAMETERS=--av=1 --threads=4 --cpu-affinity=0xAA
-SET PROGRAM_GPU_PARAMETERS=--cuda-devices=0 --cuda-launch=16x4 --cuda-bfactor=6 --cuda-bsleep=50
+
+REM SETTINGS FOR: INTEL Core i7 3770k-4770k-7770k:
+SET PROGRAM_CPU_PARAMETERS=--av=1 --threads=4 --cpu-affinity=0xAA --cpu-priority=5
+REM SETTINGS FOR: 2 x NVIDIA GTX 750ti:
+SET PROGRAM_NVIDIA_PARAMETERS=--cuda-launch=24x10 --cuda-bfactor=6 --cuda-bsleep=50
+REM SETTINGS FOR: NVIDIA GTX 960:
+rem SET PROGRAM_NVIDIA_PARAMETERS=--cuda-devices=0 --cuda-launch=32x24 --cuda-bfactor=6 --cuda-bsleep=50
+REM SETTINGS FOR: NVIDIA GTX TITAN:
+rem SET PROGRAM_NVIDIA_PARAMETERS=--cuda-devices=0 --cuda-launch=32x16 --cuda-bfactor=6 --cuda-bsleep=50
+REM SETTINGS FOR: AMD Radeon RX Vega 64:
+SET PROGRAM_AMD_PARAMETERS=--opencl-devices=0 --opencl-launch=2016x8 --opencl-platform=2 --opencl-devices=0 --opencl-launch=1600x8 --opencl-platform=2
+
+SET PROGRAM_CPU_DIFF=10000
+SET PROGRAM_NVIDIA_DIFF=20000
+SET PROGRAM_AMD_DIFF=50000
 
 SET TASKKILL=%SystemRoot%\System32\taskkill.exe
 SET TASKLIST=%SystemRoot%\System32\tasklist.exe
 SET CSCRIPT=%SystemRoot%\System32\cscript.exe
+SET TIMEOUT=%SystemRoot%\System32\timeout.exe
+REM DEVCON you can find here: https://networchestration.wordpress.com/2016/07/11/how-to-obtain-device-console-utility-devcon-exe-without-downloading-and-installing-the-entire-windows-driver-kit-100-working-method/
+REM 32: https://download.microsoft.com/download/7/D/D/7DD48DE6-8BDA-47C0-854A-539A800FAA90/wdk/Installers/82c1721cd310c73968861674ffc209c9.cab
+REM Download 82c1721cd310c73968861674ffc209c9.cab, extract the file “fil5a9177f816435063f779ebbbd2c1a1d2”, and rename it to “devcon.exe”. (download size: 7.09 MB)
+REM 64: https://download.microsoft.com/download/7/D/D/7DD48DE6-8BDA-47C0-854A-539A800FAA90/wdk/Installers/787bee96dbd26371076b37b13c405890.cab
+REM Download 787bee96dbd26371076b37b13c405890.cab, extract the file “filbad6e2cce5ebc45a401e19c613d0a28f”, and rename it to “devcon.exe”. (download size: 7.53 MB)
+SET DEVCON=%SRC%\devcon.exe
 
 IF "%1" EQU "ELEVATE" (
 	CALL :SELECT %2 %3
@@ -35,10 +55,15 @@ IF "%PARAMETER%" EQU "CPU" (
 	SET PROGRAM_FILENAME=%PROGRAM_CPU_FILENAME%
 	SET PROGRAM_PARAMETERS=%PROGRAM_PARAMETERS% %PROGRAM_CPU_PARAMETERS%
 )
-IF "%PARAMETER%" EQU "GPU" (
-	SET PROGRAM_PATH=%PROGRAM_PATH%\GPU
-	SET PROGRAM_FILENAME=%PROGRAM_GPU_FILENAME%
-	SET PROGRAM_PARAMETERS=%PROGRAM_PARAMETERS% %PROGRAM_GPU_PARAMETERS%
+IF "%PARAMETER%" EQU "NVIDIA" (
+	SET PROGRAM_PATH=%PROGRAM_PATH%\NVIDIA
+	SET PROGRAM_FILENAME=%PROGRAM_NVIDIA_FILENAME%
+	SET PROGRAM_PARAMETERS=%PROGRAM_PARAMETERS% %PROGRAM_NVIDIA_PARAMETERS%
+)
+IF "%PARAMETER%" EQU "AMD" (
+	SET PROGRAM_PATH=%PROGRAM_PATH%\AMD
+	SET PROGRAM_FILENAME=%PROGRAM_AMD_FILENAME%
+	SET PROGRAM_PARAMETERS=%PROGRAM_PARAMETERS% %PROGRAM_AMD_PARAMETERS%
 )
 CLS
 IF "%PROGRAM_FILENAME%" EQU "" GOTO END
@@ -61,8 +86,10 @@ GOTO END
 	IF "%~1" NEQ "" (
 		IF "%~1" EQU "CPU" (
 			SET PARAMETER=CPU
-		) ELSE IF "%~1" EQU "GPU" (
-			SET PARAMETER=GPU
+		) ELSE IF "%~1" EQU "NVIDIA" (
+			SET PARAMETER=NVIDIA
+		) ELSE IF "%~1" EQU "AMD" (
+			SET PARAMETER=AMD
 		) ELSE IF "%~1" EQU "START" (
 			SET ACTION=START
 		) ELSE IF "%~1" EQU "STOP" (
@@ -72,8 +99,10 @@ GOTO END
 	IF "%~2" NEQ "" (
 		IF "%~2" EQU "CPU" (
 			SET PARAMETER=CPU
-		) ELSE IF "%~2" EQU "GPU" (
-			SET PARAMETER=GPU
+		) ELSE IF "%~2" EQU "NVIDIA" (
+			SET PARAMETER=NVIDIA
+		) ELSE IF "%~2" EQU "AMD" (
+			SET PARAMETER=AMD
 		) ELSE IF "%~2" EQU "START" (
 			SET ACTION=START
 		) ELSE IF "%~2" EQU "STOP" (
@@ -82,14 +111,16 @@ GOTO END
 	)
 	IF "%ALLOW_MANUAL_SELECT%" NEQ "true" GOTO END
 	IF "%PARAMETER%" EQU "" (
-		SET /P PARAMETER="Please select a program [CPU/GPU]: "
+		SET /P PARAMETER="Please select a program [CPU/NVIDIA/AMD]: "
 	)
 	IF "%PARAMETER%" NEQ "CPU" (
-		IF "%PARAMETER%" NEQ "GPU" (
-			SET PARAMETER=
-			ECHO Select is not correct. Please input "CPU" or "GPU". Try again...
-			ECHO.
-			GOTO :SELECT
+		IF "%PARAMETER%" NEQ "NVIDIA" (
+			IF "%PARAMETER%" NEQ "AMD" (
+				SET PARAMETER=
+				ECHO Select is not correct. Please input "CPU", "NVIDIA" or "AMD". Try again...
+				ECHO.
+				GOTO :SELECT
+			)
 		)
 	)
 	IF "%ACTION%" EQU "" (
@@ -136,12 +167,26 @@ GOTO END
 	CALL :CHECK
 	IF "%ACTION%" EQU "STOP" GOTO END
 	SET USER=%WALLET%
+	IF "%PARAMETER%" EQU "CPU" SET DIFF=%PROGRAM_CPU_DIFF%
+	IF "%PARAMETER%" EQU "NVIDIA" SET DIFF=%PROGRAM_NVIDIA_DIFF%
+	IF "%PARAMETER%" EQU "AMD" SET DIFF=%PROGRAM_AMD_DIFF%
 	IF "%DIFF%" NEQ "" SET USER=%USER%+%DIFF%
 	SET PASSWORD=%ID%-%PARAMETER%
 	IF "%EMAIL%" NEQ "" SET PASSWORD=%PASSWORD%:%EMAIL%
 	SET PROGRAM_PARAMETERS=--user=%USER% --pass=%PASSWORD% %PROGRAM_PARAMETERS%
 	CD "%PROGRAM_PATH%"
 	IF "%~1" EQU "ELEVATE" (
+		IF "%PARAMETER%" EQU "AMD" (
+			IF EXIST "%DEVCON%" (
+				CALL "%DEVCON%" disable "PCI\VEN_1002&DEV_687F" > NUL
+				CALL "%TIMEOUT%" /T 5 /NOBREAK > NUL
+				CALL "%DEVCON%" enable "PCI\VEN_1002&DEV_687F" > NUL
+				CLS
+			) ELSE (
+				ECHO.
+				ECHO ERROR: Can not find "%DEVCON%".
+			)
+		)
 		CALL "%PROGRAM_PATH%\%PROGRAM_FILENAME%" %PROGRAM_PARAMETERS%
 	) ELSE (
 		START "%PROGRAM_TITLE%" /D "%PROGRAM_PATH%" "%PROGRAM_FILENAME%" %PROGRAM_PARAMETERS%
